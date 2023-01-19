75769c670b8057e9f243e65cf221d7f6d206527a
MontanchezJuan <121134352+MontanchezJuan@users.noreply.github.com>
2023-01-19 17:02:35 -0500

    Add files via upload

---
+++ Clase.java
@@ -0,0 +1,24 @@
+package com.example;
+
+public class Clase {
+    public static void main(String[] args) throws Exception {
+
+        String texto = "\nRecordatorio: Los tipos de datos más comunes son int, long, double, boolean, String.";
+        System.out.println(texto);
+
+        int numeroInt = 10;
+        System.out.println("numero int = " + numeroInt);
+
+        long numeroLong = 100300005;
+        System.out.println("numero long = " + numeroLong);
+
+        double numeroDouble = 10.9;
+        System.out.println("numero double = " + numeroDouble);
+
+        boolean verdadero = true;
+        System.out.println("Camilo está casado = " + verdadero);
+
+        String texto1 = "Adios";
+        System.out.println(texto1);
+    }
+}
